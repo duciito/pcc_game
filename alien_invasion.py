d352fc8ce3ebb6903641eb6b6d9ae0273e22747b
duciito <duciito@gmail.com>
2019-01-26 19:30:10 +0200

    add ship movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,8 @@ def run_game():
     while True:
 
         # Watch for keyboard and mouse events.
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
