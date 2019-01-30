507e7f63ed092863b732f680986ba46ddd9aa732
duciito <duciito@gmail.com>
2019-01-30 19:30:43 +0200

    create a bullet group (fix ship.py)

--- ship.py
+++ ship.py
@@ -14,7 +14,7 @@ class Ship:
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # Start each new ship at the bottom center of the screen.
+        # start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
